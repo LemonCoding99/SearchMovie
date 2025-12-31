f0aa43246263b6126622c8f08b50c43e9980ab77
chan <zinchan1@naver.com>
2025-12-31 19:31:09 +0900

    ✨ feat: user entity 생성, userRole enum 생성

--- src/main/java/com/searchmovie/domain/user/entity/User.java
+++ src/main/java/com/searchmovie/domain/user/entity/User.java
@@ -1,4 +1,23 @@
 package com.searchmovie.domain.user.entity;
 
-public class User {
+import com.searchmovie.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "users")
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private UserRole role = UserRole.USER;
 }
---
+++ src/main/java/com/searchmovie/domain/user/entity/UserRole.java
@@ -0,0 +1,6 @@
+package com.searchmovie.domain.user.entity;
+
+public enum UserRole {
+    ADMIN,
+    USER
+}
