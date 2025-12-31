1f63f1970d170550f2eae7e766b69cd32a87a364
LemonCoding99 <qordmstj7530@gmail.com>
2025-12-31 17:22:21 +0900

    ✨ feat: 전역예외처리

--- src/main/java/com/searchmovie/common/entity/BaseEntity.java
+++ src/main/java/com/searchmovie/common/entity/BaseEntity.java
@@ -1,4 +1,25 @@
 package com.searchmovie.common.entity;
 
-public class BaseEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass   //매핑정보만 상속받는 SuperClass
+@EntityListeners(AuditingEntityListener.class)
+
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;    //생성일
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;   //수정일
 }
