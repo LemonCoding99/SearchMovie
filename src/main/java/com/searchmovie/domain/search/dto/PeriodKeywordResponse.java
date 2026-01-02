72baa47769b560e79f09874e25a81de5af535e2b
jyop1212hy <jyop1212@icloud.com>
2026-01-03 04:12:41 +0900

    ✨ feat: searchLogp
    -종합 인기검색어 TOP 10
    -장르별 인기검색어 TOP 10
    -월간 인기검색어 TOP 10
    -인기검색어용 더미데이터 생성

---
+++ src/main/java/com/searchmovie/domain/search/dto/PeriodKeywordResponse.java
@@ -0,0 +1,19 @@
+package com.searchmovie.domain.search.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@RequiredArgsConstructor
+public class PeriodKeywordResponse {
+
+    private final int rank;
+    private final String keyword;
+    private final String title;
+    private final String genre;
+    private final String director;
+    private final LocalDate releaseDate;
+    private final Long score;
+}
