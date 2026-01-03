276951b51024e6c6dab9eb5861e0ded611195fa8
chan <zinchan1@naver.com>
2026-01-04 07:33:26 +0900

    🐛 fix: EnableJpaAuditing에 의해 test가 작동되지 않는 문제를 수정

--- src/main/java/com/searchmovie/SearchMovieApplication.java
+++ src/main/java/com/searchmovie/SearchMovieApplication.java
@@ -2,8 +2,12 @@ package com.searchmovie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@Profile("!test")
+@Configuration
 @EnableJpaAuditing
 @SpringBootApplication
 public class SearchMovieApplication {
@@ -11,5 +15,4 @@ public class SearchMovieApplication {
     public static void main(String[] args) {
         SpringApplication.run(SearchMovieApplication.class, args);
     }
-
 }
