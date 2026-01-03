126b83f29c0a3a5978581798f037456fcc2cb8b4
jyop1212hy <jyop1212@icloud.com>
2026-01-03 22:57:10 +0900

    🐛 fix: git 최신화 작업

--- src/main/java/com/searchmovie/common/enums/ExceptionCode.java
+++ src/main/java/com/searchmovie/common/enums/ExceptionCode.java
@@ -24,8 +24,6 @@ public enum ExceptionCode {
     // review
     REVIEW_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 해당 영화에 리뷰를 작성했습니다."),
     REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 리뷰입니다."),
-
-    REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 리뷰입니다."),
     INVALID_REVIEW_SORT(HttpStatus.BAD_REQUEST, "정렬 조건이 올바르지 않습니다. (createdAt,desc | rating,desc"),
 
 
