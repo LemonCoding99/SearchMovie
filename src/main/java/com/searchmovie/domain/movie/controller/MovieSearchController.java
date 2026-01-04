3ad7d834ba8de9fe820a369e695f472ceccbf8ac
LemonCoding99 <qordmstj7530@gmail.com>
2026-01-04 12:56:53 +0900

    ✨ feat: 검색 로그 저장 api 제작
    사용자가 검색한 검색 결과를 바탕으로 키워드, userId, movieId 받아와서 로그로 저장

--- src/main/java/com/searchmovie/domain/movie/controller/MovieSearchController.java
+++ src/main/java/com/searchmovie/domain/movie/controller/MovieSearchController.java
@@ -1,17 +1,14 @@
 package com.searchmovie.domain.movie.controller;
 
 import com.searchmovie.common.model.CommonResponse;
+import com.searchmovie.domain.movie.model.response.MovieSelectCreateResponse;
 import com.searchmovie.domain.movie.model.response.MovieSearchResponse;
 import com.searchmovie.domain.movie.model.response.SimplePageResponse;
 import com.searchmovie.domain.movie.service.MovieSearchService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
@@ -22,6 +19,7 @@ public class MovieSearchController {
 
     private final MovieSearchService movieSearchService;
 
+    // 영화 전체 검색 Api
     @GetMapping("/v2/movies/search")
     public ResponseEntity<CommonResponse<SimplePageResponse<MovieSearchResponse>>> searchMovieApi(
             @RequestParam(required = false) String title,
@@ -32,12 +30,22 @@ public class MovieSearchController {
             @RequestParam(defaultValue = "1") int page,
             @RequestParam(defaultValue = "10") int size
     ) {
-        Page<MovieSearchResponse> result =
-                movieSearchService.searchMovie(title, director, genreKeyword, releaseDateStart, releaseDateEnd, page, size);
+        SimplePageResponse<MovieSearchResponse> response = movieSearchService.searchMovie(title, director, genreKeyword, releaseDateStart, releaseDateEnd, page, size);
 
-        return ResponseEntity.ok(CommonResponse.success(SimplePageResponse.from(result), "success"));
+        return ResponseEntity.ok(CommonResponse.success(response, "success"));
     }
 
 
+    // 사용자가 검색 결과를 바탕으로 선택한 영화를 로그로 저장하는 Api
+    @PostMapping("/movies/{userId}/{movieId}/select")
+    public ResponseEntity<CommonResponse<MovieSelectCreateResponse>> createSelectApi(
+            @RequestParam String keyword,
+            @PathVariable Long userId,
+            @PathVariable Long movieId
+    ) {
+        MovieSelectCreateResponse response = movieSearchService.createSelect(keyword, userId, movieId);
+
+        return ResponseEntity.ok(CommonResponse.success(response, "success"));
+    }
 
 }
--- src/main/java/com/searchmovie/domain/movie/service/MovieSearchService.java
+++ src/main/java/com/searchmovie/domain/movie/service/MovieSearchService.java
@@ -1,7 +1,16 @@
 package com.searchmovie.domain.movie.service;
 
+import com.searchmovie.common.enums.ExceptionCode;
+import com.searchmovie.common.exception.CustomException;
+import com.searchmovie.domain.movie.model.response.MovieSelectCreateResponse;
+import com.searchmovie.domain.movie.entity.Movie;
 import com.searchmovie.domain.movie.model.response.MovieSearchResponse;
+import com.searchmovie.domain.movie.model.response.SimplePageResponse;
 import com.searchmovie.domain.movie.repository.MovieRepository;
+import com.searchmovie.domain.search.entity.SearchLog;
+import com.searchmovie.domain.search.repository.SearchRepository;
+import com.searchmovie.domain.user.entity.User;
+import com.searchmovie.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -16,12 +25,38 @@ import java.time.LocalDate;
 public class MovieSearchService {
 
     private final MovieRepository movieRepository;
+    private final UserRepository userRepository;
+    private final SearchRepository searchRepository;  // 나중에 SearchLogRepository로 변경
 
+    // 영화 전체 검색
     @Transactional(readOnly = true)
-    public Page<MovieSearchResponse> searchMovie(String title, String director, String genreKeyword, LocalDate releaseDateStart, LocalDate releaseDateEnd, int page, int size) {
+    public SimplePageResponse<MovieSearchResponse> searchMovie(String title, String director, String genreKeyword, LocalDate releaseDateStart, LocalDate releaseDateEnd, int page, int size) {
 
         Pageable pageable = PageRequest.of(page - 1, size);
-        return movieRepository.search(title, director, genreKeyword, releaseDateStart, releaseDateEnd, pageable);
+        Page<MovieSearchResponse> response = movieRepository.search(title, director, genreKeyword, releaseDateStart, releaseDateEnd, pageable);
+
+        return SimplePageResponse.from(response);
+
     }
+
+
+    // 영화 검색 로그 생성
+    @Transactional
+    public MovieSelectCreateResponse createSelect(String keyword, Long userId, Long movieId) {
+
+        if (keyword == null || keyword.trim().isEmpty()) {
+            throw new IllegalArgumentException("검색어는 꼭 포함되어야 합니다.");  // custom으로 변경해주기➕
+        }
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(ExceptionCode.USER_NOT_FOUND));
+        Movie movie = movieRepository.findById(movieId)
+                .orElseThrow(() -> new CustomException(ExceptionCode.MOVIE_NOT_FOUND));
+
+        SearchLog newSearchLog = searchRepository.save(new SearchLog(user, movie, keyword));
+
+        return MovieSelectCreateResponse.from(newSearchLog);
+    }
+
+
 
 }
