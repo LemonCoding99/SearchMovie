ba0dc4c403d543c7a58017568bc42bba3348071b
LemonCoding99 <qordmstj7530@gmail.com>
2026-01-07 09:36:52 +0900

    ♻ refactor: Redis 캐시 사용 시 CacheHit되지 않는 문제 해결
    캐시가 DB에 저장되고 역직렬화하면서 발생하던 문제 해결

--- src/main/java/com/searchmovie/domain/movie/model/response/MovieSearchResponse.java
+++ src/main/java/com/searchmovie/domain/movie/model/response/MovieSearchResponse.java
@@ -2,16 +2,18 @@ package com.searchmovie.domain.movie.model.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor // Redis 역직렬화 시 객체를 생성할 수 있게 함
+@AllArgsConstructor // 기존 생성 흐름 유지용
 public class MovieSearchResponse {
-    private final Long id;
-    private final String title;
-    private final List<String> genres;  // 모든 장르 가져오기
-    private final String director;
-    private final LocalDate releaseDate;
+    private Long id;
+    private String title;
+    private List<String> genres;  // 모든 장르 가져오기
+    private String director;
+    private LocalDate releaseDate;
 }
